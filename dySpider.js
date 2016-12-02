94d77d04fba288db98d190bc657cbb2dc274fb52
xia <222@dd.com>
2016-12-02 22:05:36 +0800

    提交

--- dySpider.js
+++ dySpider.js
@@ -10,13 +10,15 @@ var eventproxy = require('eventproxy');
 var charset = require('superagent-charset');
 var superagent = charset(require('superagent'));
 
+var Page = require("./db/page.js");
+
 
 var catchFirstUrl = 'http://www.cnblogs.com/',	//入口页面
 	deleteRepeat = {},	//去重哈希数组
 	urlsArray = [],	//存放爬取网址
 	catchDate = [],	//存放爬取数据
 	pageUrls = [],	//存放收集文章页面网站
-	pageNum = 1000,	//要爬取文章的页数
+	pageNum = 100,	//要爬取文章的页数
 	startDate = new Date(),	//开始时间
 	endDate = false;	//结束时间
 
@@ -32,6 +34,8 @@ for (var i = 0; i <= pageNum; i++) {
 	pageUrls.push('http://www.dy2018.com/i/' + (iStart + i) + '.html');
 }
 
+//pageUrls = ['http://www.dy2018.com/i/96458.html']
+
 // 主start程序
 function requestHtml(pageUrl, callback) {
 	superagent.get(pageUrl).charset('gb2312')
@@ -39,7 +43,6 @@ function requestHtml(pageUrl, callback) {
 
 			//res.write('fetch ' + pageUrl + ' successful<br/>');
 			callback(null, pageUrl)
-
 			// 常规的错误处理
 			if (err) {
 
@@ -55,6 +58,7 @@ function requestHtml(pageUrl, callback) {
 
 			if (pres) {
 				var html = pres.text;
+
 				var $ = cheerio.load(html);
 				//console.log(html)
 
@@ -67,15 +71,44 @@ function requestHtml(pageUrl, callback) {
 				var topicImg = $('img[class="topic"]')
 
 
-				console.log(curPageUrls.text())
+				console.log(getText(curPageUrls), curPageUrls.length)
 
-				console.log(fileUrl.text())
+				console.log(getText(fileUrl), fileUrl.length)
 
+				var page = new Page({
+					title: curPageUrls,
+					fileUrl: '',
+					desc: desc,
+					topicImg: topicImg
+				});
+				page.save(function (err, res) {
+
+					if (err) {
+						console.log("Error:" + err);
+					}
+					else {
+						console.log("Res:" + res);
+					}
+
+				});
 
 				//console.log(topicImg.attr("src"))
 				//
 				//console.log(desc.html())
+				function getText(element) {
+					if (element.length > 1) {
 
+						var ret = []
+						element.map(function (i, el) {
+							ret.push($(el).text())
+						})
+						return ret
+					} else {
+						return element.text()
+
+					}
+
+				}
 			}
 
 		})
@@ -86,11 +119,10 @@ function getPage(res) {
 	//	requestHtml(pageUrl);
 	//})
 
-
 	async.mapLimit(pageUrls, 20, requestHtml, function (err, results) {
-
 
 	})
 }
+
 
 getPage()
